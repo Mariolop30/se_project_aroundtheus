822aa04c28b1f4355b587e7af1f1844784cd8a5a
Mario <pezmeisters@gmail.com>
2024-04-28 08:52:20 -0700

    added more js code and fixed modal save button in html

--- scripts/index.js
+++ scripts/index.js
@@ -33,11 +33,40 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseModal = document.querySelector("#profile-close-modal");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const profileTitleInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-title-description"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
 
+/*----------------------------------------------------------------------------*/
+/*                                  Functions                                 */
+/*----------------------------------------------------------------------------*/
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                  Event Handlers                            */
+/*----------------------------------------------------------------------------*/
+function handleProfileEditSumbit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*----------------------------------------------------------------------------*/
+/*                                  Event Listeners                           */
+/*----------------------------------------------------------------------------*/
 profileEditButton.addEventListener("click", () => {
+  profileTitleInput.value = profileTitle.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-profileCloseModal.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
-});
+profileCloseModal.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSumbit);
